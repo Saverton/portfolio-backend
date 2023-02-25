c55d391e40705a48185614f7a95d0e81c1125140
Scott Meadows <Scottmeadows04@gmail.com>
2023-02-24 22:27:46 -0500

    feat: add project submit form
    Functional project submit form complete. Still needs styling and to be locked on it's own page, accessible to admin users only.

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -3,6 +3,7 @@
   import About from '../About.svelte';
   import Portfolio from '../Portfolio.svelte';
   import Contact from '../Contact.svelte';
+  import ProjectForm from '../ProjectForm.svelte';
 </script>
 
 <main>
@@ -10,4 +11,5 @@
   <About />
   <Portfolio />
   <Contact />
+  <ProjectForm />
 </main>
