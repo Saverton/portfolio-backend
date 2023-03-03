dab03a2fa192de1b6706c15996fef49f5c0bfc23
Scott Meadows <Scottmeadows04@gmail.com>
2023-03-03 16:48:35 -0500

    fix: session not available in prod
    tweak session_store.rb configuration to try to fix.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,10 @@
 Rails.application.config.session_store :cookie_store, {
-  key: '_application_session',
+  key: '_session_id',
   domain: :all,
   same_site: :none,
   secure: true
 }
+
+Rails.application.config.action_dispatch.cookies_same_site_protection = :none
+
+Rails.application.config.force_ssl = true
