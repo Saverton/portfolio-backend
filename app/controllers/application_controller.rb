adb0c8cc2ed63388485e96f9830a1b6892c8ef93
Scott Meadows <Scottmeadows04@gmail.com>
2023-03-03 15:57:30 -0500

    fix: CSRF protection doesn't block requests
    CSRF protection implemented semi-manually, since different domains were causing conflicts.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,25 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   include ActionController::Cookies
+  after_action :set_csrf_cookie
+  skip_before_action :verify_authenticity_token
 
   private
 
   def authorize
-    return unauthorized unless session[:admin_id]
+    return unauthorized unless
+      cookies['CSRF_TOKEN'] == request.headers['X-CSRF-Token'] &&
+      session[:admin_id]
   end
 
   def unauthorized
     render json: { error: 'Not authorized' }, status: :unauthorized
+  end
+
+  def set_csrf_cookie
+    cookies['CSRF_TOKEN'] = {
+      value: form_authenticity_token,
+      secure: true,
+      same_site: :strict
+    }
   end
 end
